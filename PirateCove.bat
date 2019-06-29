d5d9d3be0fe73183765e0eef46096a6e250c7c67
ConanHUN <conan_513@hotmail.com>
2019-06-29 21:57:44 +0200

    Test

---
+++ PirateCove.bat
@@ -0,0 +1,10 @@
+@echo off
+set name=Single Player Project - Pirate Cove
+TITLE %NAME%
+set mainfolder=%CD%
+
+echo #######################################################
+echo # Single Player Project - Pirate Cove                 #
+echo # Patreon: https://www.patreon.com/conan513           #
+echo # Discord: https://discord.gg/TpxqWWT                 #
+echo #######################################################
